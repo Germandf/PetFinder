42671e7587563ed6d29ccc8c7ced86b765efa2f0
jose.huertas@eternet.cc <jhuertasdeveloper@gmail.com>
2020-10-04 18:28:34 -0300

    Muestro el navbar y agrego un menucontroller

--- router.php
+++ router.php
@@ -3,11 +3,15 @@
 // defino la base url para la construccion de links con urls semánticas
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
+/* El menu esta pensado para ser el controlador 
+que "navega" entre las diferentes secciones */
+include_once('app/controllers/menu.controller.php');
+$menu = new  MenuController();
 // lee la acción
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 } else {
-    $action = 'index'; // acción por defecto si no envían
+    $action = 'listar'; // acción por defecto si no envían
 }
 
 // parsea la accion Ej: suma/1/2 --> ['suma', 1, 2]
@@ -16,25 +20,25 @@ $params = explode('/', $action);
 // determina que camino seguir según la acción
 switch ($params[0]) {
     case 'index':
-        echo 'Index';
+        $menu->showNavBar();
         break;
     case 'admin':
-        echo 'Admin';
+        echo('Admin');
         break;
     case 'categories':
-        echo 'Categories';
+        echo('Categories');
         break;
     case 'about':
-        echo 'About';
+        echo('About');
         break;
     case 'login':
-        echo 'Login';
+        echo('Login');
         break;
     case 'signup':
-        echo 'Signup';
+        echo('Signup');
         break;
     default:
         header("HTTP/1.0 404 Not Found");
-        echo '404 Page not found';
+        echo('404 Page not found');
         break;
 }
