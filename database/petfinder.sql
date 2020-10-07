32c9f6be21c0703ef6773773d7824672ba45f315
Germandf <germandefrancesco@hotmail.com>
2020-10-07 19:36:50 -0300

    Reestructuracion de la database, eliminamos tabla lost_type, agregamos tabla user_permission

--- database/petfinder.sql
+++ database/petfinder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2020 a las 04:12:41
+-- Tiempo de generación: 08-10-2020 a las 00:34:18
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -35,10 +35,10 @@ CREATE TABLE `animal_type` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cities`
+-- Estructura de tabla para la tabla `city`
 --
 
-CREATE TABLE `cities` (
+CREATE TABLE `city` (
   `id` int(11) NOT NULL,
   `name` varchar(35) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -57,46 +57,47 @@ CREATE TABLE `gender` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `lost_pets`
+-- Estructura de tabla para la tabla `pet`
 --
 
-CREATE TABLE `lost_pets` (
+CREATE TABLE `pet` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `animal_type_id` int(11) NOT NULL,
   `city_id` int(11) NOT NULL,
-  `lost_type_id` int(11) NOT NULL,
   `gender_id` int(11) NOT NULL,
   `date` date NOT NULL,
   `phone_number` int(11) NOT NULL,
   `photo` varchar(30) NOT NULL,
   `description` varchar(200) DEFAULT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `found` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `lost_type`
+-- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `lost_type` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `surname` varchar(20) NOT NULL,
   `email` varchar(254) NOT NULL,
-  `password` varchar(65) NOT NULL
+  `password` varchar(65) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_permission`
+--
+
+CREATE TABLE `user_permission` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -110,9 +111,9 @@ ALTER TABLE `animal_type`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `cities`
+-- Indices de la tabla `city`
 --
-ALTER TABLE `cities`
+ALTER TABLE `city`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,26 +123,26 @@ ALTER TABLE `gender`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `lost_pets`
+-- Indices de la tabla `pet`
 --
-ALTER TABLE `lost_pets`
+ALTER TABLE `pet`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `lost_type_id` (`lost_type_id`),
   ADD KEY `gender_id` (`gender_id`),
   ADD KEY `city_id` (`city_id`),
   ADD KEY `animal_type_id` (`animal_type_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indices de la tabla `lost_type`
+-- Indices de la tabla `user`
 --
-ALTER TABLE `lost_type`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `permission_id` (`permission_id`);
 
 --
--- Indices de la tabla `users`
+-- Indices de la tabla `user_permission`
 --
-ALTER TABLE `users`
+ALTER TABLE `user_permission`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -155,9 +156,9 @@ ALTER TABLE `animal_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `cities`
+-- AUTO_INCREMENT de la tabla `city`
 --
-ALTER TABLE `cities`
+ALTER TABLE `city`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -167,21 +168,21 @@ ALTER TABLE `gender`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `lost_pets`
+-- AUTO_INCREMENT de la tabla `pet`
 --
-ALTER TABLE `lost_pets`
+ALTER TABLE `pet`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `lost_type`
+-- AUTO_INCREMENT de la tabla `user`
 --
-ALTER TABLE `lost_type`
+ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT de la tabla `user_permission`
 --
-ALTER TABLE `users`
+ALTER TABLE `user_permission`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -189,14 +190,19 @@ ALTER TABLE `users`
 --
 
 --
--- Filtros para la tabla `lost_pets`
+-- Filtros para la tabla `pet`
 --
-ALTER TABLE `lost_pets`
-  ADD CONSTRAINT `lost_pets_ibfk_1` FOREIGN KEY (`animal_type_id`) REFERENCES `animal_type` (`id`) ON UPDATE NO ACTION,
-  ADD CONSTRAINT `lost_pets_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON UPDATE NO ACTION,
-  ADD CONSTRAINT `lost_pets_ibfk_3` FOREIGN KEY (`lost_type_id`) REFERENCES `lost_type` (`id`) ON UPDATE NO ACTION,
-  ADD CONSTRAINT `lost_pets_ibfk_4` FOREIGN KEY (`gender_id`) REFERENCES `gender` (`id`) ON UPDATE NO ACTION,
-  ADD CONSTRAINT `lost_pets_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION;
+ALTER TABLE `pet`
+  ADD CONSTRAINT `pet_ibfk_1` FOREIGN KEY (`animal_type_id`) REFERENCES `animal_type` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pet_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pet_ibfk_4` FOREIGN KEY (`gender_id`) REFERENCES `gender` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pet_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `user_permission` (`id`) ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
